548bbfc435314149a89bbfdf1b9f666d06e8d01d
ianguest <iag.1.apps@gmail.com>
2021-05-14 13:16:26 +0100

    feat: Add avatar to Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,16 +3,21 @@ import { connect } from 'react-redux'
 
 class Question extends React.Component {
   render() {
-    const { question } = this.props;
-    if (question === null) {
+    const { question, avatarURL } = this.props;
+    if (!question) {
       return <p>This Question does not exist</p>;
     }
     return (
-      <div>
-        <h3>{`${question.author} asks:`}</h3>
-        <p>Would you rather?</p>
-        <p>1. { question.optionOne.text }</p>
-        <p>2. { question.optionTwo.text }</p>
+      <div style={{ display: "grid", gridTemplateColumns: "10% 90%", gridGap: 20 }}>
+        <div>
+          <img src={ avatarURL } alt="" style={{width:"100px", height:"100px"}}/>
+        </div>
+        <div>
+          <h3>{`${question.author} asks:`}</h3>
+          <p>Would you rather?</p>
+          <p>1. { question.optionOne.text }</p>
+          <p>2. { question.optionTwo.text }</p>
+        </div>
       </div>
     );
   }
@@ -22,9 +27,8 @@ function mapStateToProps({authedUser, users, questions}, { id }) {
   const question = questions[id];
   return {
     authedUser,
-    question: question
-      ? question
-      : null
+    question: question,
+    avatarURL: question ? users[question.author].avatarURL : null
   };
 }
 
