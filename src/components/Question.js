6fe3859ca3fc4c482bfcf09de7e6b1a0a245724e
ianguest <iag.1.apps@gmail.com>
2021-05-16 14:03:36 +0100

    feat: Add controls to question
    Radio buttons and submit

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,18 +2,43 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class Question extends React.Component {
+  optionOneStr = "option1";
+  optionTwoStr = "option2";
+  state = {
+    selectedOption: this.optionOneStr
+  }
+  onSelectOption = e => {
+    this.setState(()=>({
+      selectedOption: e.target.value
+    }));
+  }
+  onSubmit = () => {
+    console.log("Selected option: ", this.state.selectedOption)
+    // TODO: Update store
+  }
   render() {
     const { question, avatarURL } = this.props;
     return (
-      <div style={{ display: "grid", gridTemplateColumns: "10% 90%", gridGap: 20, alignItems: 'center' }}>
+      <div style={{ display: "grid", gridTemplateColumns: "10% 90%", gridGap: 20, alignItems: 'center', border: "1px solid black", margin: "10px" }}>
         <div>
           <img src={ avatarURL } alt="" style={{width:"75%", height:"75%"}}/>
         </div>
         <div>
-          <h3>{`${question.author} asks:`}</h3>
+          <h2>{`${question.author} asks:`}</h2>
           <p>Would you rather?</p>
-          <p>1. { question.optionOne.text }</p>
-          <p>2. { question.optionTwo.text }</p>
+          <div>
+            <form>
+              <input type="radio" id="choiceOne" name="contact" value={this.optionOneStr} checked={this.state.selectedOption === this.optionOneStr} onChange={this.onSelectOption} />
+              <label>1. { question.optionOne.text }</label>
+              <br/>
+              <input type="radio" id="choiceTwo" name="contact" value={this.optionTwoStr} checked={this.state.selectedOption === this.optionTwoStr} onChange={this.onSelectOption} />
+              <label>2. { question.optionTwo.text }</label>
+            </form>
+          </div>
+          <br/>
+          <div>
+            <button type="submit" onClick={this.onSubmit}>Submit</button>
+          </div>
         </div>
       </div>
     );
