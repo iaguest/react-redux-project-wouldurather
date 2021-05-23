6d4d6d10d971a8cdd940f3cdf44b7c307fb7ce36
ianguest <iag.1.apps@gmail.com>
2021-05-23 20:55:04 +0100

    feat: Display stats for QuestionResult
    Very basic display atm.

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -1,11 +1,56 @@
-import { resultsString } from '../utils/strings'
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+import { optionOneString, optionTwoString, resultsString } from '../utils/strings'
+import { getSelectedOption } from '../utils/questionHelper'
 
 function QuestionResult(props) {
+  const {
+    optionOne,
+    optionTwo,
+    numVotesOption1,
+    numVotesOption2,
+    totalVotes,
+    selectedOption } = props;
   return (
     <div>
       <p>{ resultsString }:</p>
+      <p>
+        1. { optionOne }
+        { _displayVotes(numVotesOption1, totalVotes, selectedOption === optionOneString) }
+      </p>
+      <p>
+        2. { optionTwo }
+        { _displayVotes(numVotesOption2, totalVotes, selectedOption === optionTwoString) }
+      </p>
     </div>
   );
 }
 
-export default QuestionResult;
+function _displayVotes(num, total, isSelectedOption) {
+  return (
+    <p className='votes'>
+      {`${num} of ${total} votes (${((num/total)*100).toFixed(1)}%) ${(isSelectedOption) ? '*' : ''}`}
+    </p>
+  );
+}
+
+function mapStateToProps({authedUser, users, questions}, {id}) {
+  const question = questions[id];
+  const optionOne = question.optionOne;
+  const optionTwo = question.optionTwo;
+  const numVotesOption1 = optionOne.votes.length;
+  const numVotesOption2 = optionTwo.votes.length;
+  return {
+    authedUser,
+    optionOne: optionOne.text,
+    optionTwo: optionTwo.text,
+    numVotesOption1,
+    numVotesOption2,
+    totalVotes: numVotesOption1 + numVotesOption2,
+    selectedOption: getSelectedOption(users, authedUser, id),
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(QuestionResult));
