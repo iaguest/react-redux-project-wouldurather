03f04e9328db9277dd80f66968ffe173338287a0
ianguest <iag.1.apps@gmail.com>
2021-06-03 21:35:40 +0100

    fix: add missing await

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -25,11 +25,11 @@ export function handleAnswerQuestion(info) {
   return async (dispatch) => {
     dispatch(answerQuestion(info));
     try {
-      return _saveQuestionAnswer(info);
+      return await _saveQuestionAnswer(info);
     } catch (e) {
       console.warn('Error in handleAnswerQuestion: ', e);
-      // TODO: reverse setting to previous state
       alert('There was an error handling your answer. Try again!');
+      // TODO: reverse setting to previous state
     }
   }
 }
